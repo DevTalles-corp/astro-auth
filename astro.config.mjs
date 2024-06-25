5ccd696286d4be2ad43df65264bd9747007187d0
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-25 12:35:48 -0400

    chore: Add Astro integrations for Tailwind and Netlify
    This commit adds Astro integrations for Tailwind and Netlify. It updates the `astro.config.mjs` file to include the `tailwind` and `netlify` integrations. Additionally, it updates the `package.json` file to include the latest versions of `@astrojs/netlify` and `@astrojs/tailwind`. Finally, it modifies the `index.astro` file to import the `MainLayout` component from the `@/layouts/MainLayout.astro` module.
    
    ```

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
+});
