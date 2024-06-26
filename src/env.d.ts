ce9f7c2063b555d8738b45a6a64695244916b456
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-26 09:59:36 -0400

    chore: Update login page with conditional rendering for protected routes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,16 @@
 /// <reference path="../.astro/actions.d.ts" />
 /// <reference types="astro/client" />
+
+interface User {
+  email: string;
+  name: string;
+  avatar: string;
+  emailVerified: boolean;
+}
+
+declare namespace App {
+  interface Locals {
+    isLoggedIn: boolean;
+    user: User | null;
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,33 @@
 import type { MiddlewareNext } from 'astro';
 import { defineMiddleware } from 'astro:middleware';
+import { firebase } from './firebase/config';
 
 const privateRoutes = ['/protected'];
+const notAuthenticatedRoutes = ['/login', '/register'];
 
-export const onRequest = defineMiddleware(async ({ url, request }, next) => {
-  return next();
-});
+export const onRequest = defineMiddleware(
+  async ({ url, request, locals, redirect }, next) => {
+    const isLoggedIn = !!firebase.auth.currentUser;
+    const user = firebase.auth.currentUser;
+
+    locals.isLoggedIn = isLoggedIn;
+    if (user) {
+      locals.user = {
+        avatar: user.photoURL ?? '',
+        email: user.email!,
+        name: user.displayName!,
+        emailVerified: user.emailVerified,
+      };
+    }
+
+    if (!isLoggedIn && privateRoutes.includes(url.pathname)) {
+      return redirect('/');
+    }
+
+    if (isLoggedIn && notAuthenticatedRoutes.includes(url.pathname)) {
+      return redirect('/');
+    }
+
+    return next();
+  }
+);
